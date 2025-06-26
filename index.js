e92f890963a2d4ac5981f88a019524153b1ad264
Rrdesh07 <rohandeshmukh998@gmail.com>
2025-06-26 23:38:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//logical operators
+// Note : The logical operators contain two operators && and || 
+
+// && is logical AND opertor for which both the operands should be true to return true
+// || is logical OR operator for which at least one of the operands should be true to return true
+
+a=true;
+b=false;
+console.log( a && b);  //falsse because b is false 
+
+console.log( a || b);  //true because a is true
